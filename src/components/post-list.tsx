ce04d4b739083a284dc759dd3742bc3bf14ccb38
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-06-21 18:48:56 +0530

    fix: typescript error

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
 const PostList = async() => {
     const response = await fetch("https://dummyjson.com/posts?limit=10")
-    const data = await response.json()
+    const data:{posts: Post[]} = await response.json()
     return (
         <ul>
             <li className="max-w-[700px] mx-auto">
                 {
-                    data.posts.map(post => (
+                    data.posts.map((post) => (
                         <li className="mb-3" key={post.id}>
                             <Link href={`/posts/${post.id}`}>{post.title}</Link>
                         </li>
