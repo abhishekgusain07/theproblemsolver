a2a30251bdafcc9b068d9ffab7cdfbe7af20099a
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-06-22 15:18:25 +0530

    feat: zod validation for form

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,4 +1,20 @@
 "use server"
+import { z } from "zod";
+
+const postSchema = z.object({
+    title: z.string().min(5, "Title is required").max(30),
+    body: z.string().min(10, "Body is required").max(500)
+});
 export async function createPost(formData: FormData) {
-     
+    const title = formData.get('title') as string;
+    const body = formData.get('body') as string;
+
+    //zod validation
+    const result = postSchema.safeParse({title, body});
+    
+    if(!result.success) {
+        console.log(result.error.issues)
+        return { error: result.error.issues };
+    }
+    console.log(title, body);    
 }
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,22 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { createPost } from "../../actions/actions"
-
+import Form from "@/components/Form"
 const CreatePost = async () => {
     return (
         <main className="text-center pt-16">
             <h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
-            <form
-                action={createPost} 
-                className="h-10 flex flex-col space-x-2 mt-10">
-                <input
-                    className="border rounded px-3 h-full"
-                    type="text" 
-                    name="title"
-                    placeholder="Title for new Post"
-                    required
-                />
-                <Button className="h-full bg-blue-500 px-5 rounded text-white">Submit</Button>
-            </form>
+            <Form / >
         </main>
     )
 }
