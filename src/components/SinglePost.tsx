e6099157f5dd7d265eb86ecf7a2951379cbb4596
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-07-01 09:02:47 +0530

    chore: postList ui updated

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -9,6 +9,14 @@ import LikeIcon from "./icons/LikeIcon";
 import CommentIcon from "./icons/CommentIcon";
 import { didUserLikedThePost } from "./dbFunctions/didUserLIkedThePost";
 
+const convertDate = (dateStr: Date):string=> {
+    const dateObj = new Date(dateStr);
+    const day = dateObj.getDate();
+    const month = dateObj.toLocaleString('default', { month: 'long' });
+    const year = dateObj.getFullYear();
+    const formattedDate = `${day} ${month} ${year}`;
+    return formattedDate
+}
 const SinglePost = ({post}: {post: PostWithCounts}) => {
     const {user, isLoaded} = useUser();
     const [isAdmin, setIsAdmin] = useState<boolean>(false)
@@ -30,23 +38,30 @@ const SinglePost = ({post}: {post: PostWithCounts}) => {
         fetchUserLiked()
     },[post, user])
     if(!isLoaded)return <></>
+    console.log('-'+post.createdAt+'-')
+    const postDate = convertDate(post.createdAt)
     return (
-        <li className="mb-3" key={post.id}>
-            <div className="flex flex-row text-center justify-between items-center space-x-3 mx-auto">
-                <Link href={`/posts/${post.id}`}>{post.title}</Link>
-                {
-                    isAdmin ? (
-                        <div className="flex items-center justify-center gap-x-3">
-                            <UpdateIcon postId={post.id} />
-                            <TrashIcon postId={post.id} />
-                        </div>
-                    ) : (
-                        <div className="flex items-center justify-between gap-x-5">
-                            <LikeIcon likes={post._count.likes} userLiked={userLiked}/>
-                            <CommentIcon comments={post._count.comments} />
-                        </div>
-                    ) 
-                }
+        <li className="mb-3 flex flex-col" key={post.id}>
+            <div className="flex flex-row text-center justify-between items-center">
+                <Link href={`/posts/${post.id}`} className="text-lg font-semibold">{post.title}</Link>
+                <div>
+                    {
+                        isAdmin ? (
+                            <div className="flex items-center justify-center gap-x-3">
+                                <UpdateIcon postId={post.id} />
+                                <TrashIcon postId={post.id} />
+                            </div>
+                        ) : (
+                            <div className="flex items-center justify-between gap-x-5">
+                                <LikeIcon likes={post._count.likes} userLiked={userLiked}/>
+                                <CommentIcon comments={post._count.comments} />
+                            </div>
+                        ) 
+                    }
+                </div>
+            </div>
+            <div className="flex flex-row items-start -mt-1">
+                <p className="text-muted-foreground text-[13px] tracking-tight">{postDate}</p>
             </div>
         </li>
     )
