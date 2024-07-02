4c9b1184df4fe2e7b5a616b0555db41704c25335
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-07-02 16:57:26 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5 h-full"> theProblemSolver </h1>
       <p className="max-w-[750px] mx-auto leading-8">
-       My journey to engineering excellence by solving real-world problems, focusing on impact over frameworks. Let's tackle real challenges together!
+       My journey to engineering excellence by solving real-world problems, focusing on impact over frameworks. Let&apos;s tackle real challenges together
       </p>
       <div className="fixed bottom-20 inset-x-0 flex justify-center items-center gap-x-2">
         <ExternalLink 
